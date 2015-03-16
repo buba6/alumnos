e37121b50427b03f27ae0b98097e95132fba1104
chuyano <spawner.astral@hotmail.com>
2015-03-16 15:49:50 -0600

    base del proyecto

---
+++ alumnoo/Program.cs
@@ -0,0 +1,25 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: buba
+ * Date: 16/03/2015
+ * Time: 03:46 p.m.
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace alumnoo
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+			
+			// TODO: Implement Functionality Here
+			
+			Console.Write("Press any key to continue . . . ");
+			Console.ReadKey(true);
+		}
+	}
+}
